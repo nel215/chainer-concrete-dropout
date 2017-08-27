a9f6797d328b1d3728a5496d824131884315940b
nel215 <otomo.yuhei@gmail.com>
2017-08-27 16:00:07 +0900

    Separate the ConcreteDropout class as a Chain

--- main.py
+++ main.py
@@ -1,57 +1,77 @@
 # coding: utf-8
-from chainer import datasets, iterators, Chain, optimizers, training, cuda, Variable, Parameter
+from chainer import datasets, iterators, Chain, optimizers, training, cuda,\
+                    Parameter
 from chainer.training import extensions
 import chainer.functions as F
 import chainer.links as L
+from chainer import link
 import numpy as np
-import cupy as cp
 
 
-class MLP(Chain):
-    def __init__(self, n_hidden, n_out):
-        super(MLP, self).__init__()
+class ConcreteDropout(link.Chain):
+
+    def __init__(self, layer):
+        super(ConcreteDropout, self).__init__()
         with self.init_scope():
-            self.pl1 = Parameter(0, (1))
-            self.l1 = L.Linear(None, n_hidden)
-            self.l2 = L.Linear(None, n_out)
+            self.layer = layer
+            self.pl = Parameter(0, (1))
 
     def __call__(self, x):
-        # Concrete Dropout
         xp = cuda.get_array_module(x)
         x = F.broadcast(x)
-        self.input_shape1 = x.shape
+        self.input_shape = x.shape
         eps = 1e-20
         temp = 0.1
-        p1 = F.sigmoid(self.pl1)
-        p1 = F.broadcast_to(p1, x.shape)
+        p = F.sigmoid(self.pl)
+        p = F.broadcast_to(p, x.shape)
         noise = xp.random.uniform(size=x.shape).astype('float32')
-        drop = F.log(p1 + eps) - F.log(1. - p1 + eps)
+        drop = F.log(p + eps) - F.log(1. - p + eps)
         drop += F.log(noise + eps) - F.log(1. - noise + eps)
         drop = F.sigmoid(drop / temp)
         x *= 1. - drop
-        x /= 1. - p1
+        x /= 1. - p
 
-        h1 = F.relu(self.l1(x))
-        y = self.l2(h1)
-        return y
+        return self.layer(x)
+
+    def get_layer_square_norm(self):
+        # TODO: consider a type of the layer
+        return F.sum(F.square(self.layer.W))
 
     def get_regularizer_loss(self):
-        input_dim = np.prod(self.input_shape1[1:])
-        p1 = F.sigmoid(self.pl1)[0]
+        input_dim = np.prod(self.input_shape[1:])
+        p = F.sigmoid(self.pl)[0]
         # TODO: hyper parameter is l**2/N
-        weight_reg = 0.001 * F.sum(F.square(self.l1.W)) / (1. - p1)
-        ber_reg = p1 * F.log(p1) + (1. - p1) * F.log(1. - p1)
+        weight_reg = 0.001 * self.get_layer_square_norm() / (1. - p)
+        ber_reg = p * F.log(p) + (1. - p) * F.log(1. - p)
         # TODO: hyper parameter is 2/N
         drop_reg = 0.001 * input_dim * ber_reg
         reg = F.sum(weight_reg + drop_reg)
         return reg
 
 
+class MLP(Chain):
+    def __init__(self, n_hidden, n_out):
+        super(MLP, self).__init__()
+        self.loss = 0
+
+        with self.init_scope():
+            self.cd1 = ConcreteDropout(L.Linear(None, n_hidden))
+            self.l2 = L.Linear(None, n_out)
+
+    def __call__(self, x):
+        h1 = F.relu(self.cd1(x))
+        y = self.l2(h1)
+
+        self.loss = 0
+        self.loss += self.cd1.get_regularizer_loss()
+        return y
+
+
 class Classifier(L.Classifier):
 
     def __call__(self, *args):
         loss = super(Classifier, self).__call__(*args)
-        loss += self.predictor.get_regularizer_loss()
+        loss += self.predictor.loss
         return loss
 
 
